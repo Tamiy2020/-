bd40c06e69d697c2244ae10e92acb52c255727e4
Tamiy <1145849201@qq.com>
2019-12-27 14:41:37 +0800

    限定文本框只能输入数字

--- 正则表达式/Form1.Designer.cs
+++ 正则表达式/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_Regex = new System.Windows.Forms.TextBox();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.lbl_Tips = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_Regex
+            // 
+            this.txt_Regex.Location = new System.Drawing.Point(24, 74);
+            this.txt_Regex.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Regex.MaxLength = 6;
+            this.txt_Regex.Name = "txt_Regex";
+            this.txt_Regex.PasswordChar = '●';
+            this.txt_Regex.Size = new System.Drawing.Size(116, 23);
+            this.txt_Regex.TabIndex = 0;
+            this.txt_Regex.TextChanged += new System.EventHandler(this.txt_Regex_TextChanged);
+            this.txt_Regex.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Regex_KeyDown);
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Location = new System.Drawing.Point(169, 74);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(75, 23);
+            this.btn_OK.TabIndex = 1;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // lbl_Tips
+            // 
+            this.lbl_Tips.AutoSize = true;
+            this.lbl_Tips.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Tips.Location = new System.Drawing.Point(24, 41);
+            this.lbl_Tips.Name = "lbl_Tips";
+            this.lbl_Tips.Size = new System.Drawing.Size(0, 17);
+            this.lbl_Tips.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(262, 131);
+            this.Controls.Add(this.lbl_Tips);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.txt_Regex);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_Regex;
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.Label lbl_Tips;
     }
 }
 
--- 正则表达式/Form1.cs
+++ 正则表达式/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Text.RegularExpressions;//引用正则表达式命名空间
+
 namespace 正则表达式
 {
     public partial class Form1 : Form
@@ -15,6 +17,60 @@ namespace 正则表达式
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 点击btn_OK按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            string str = txt_Regex.Text;
+            if (str=="190610")
+            {
+                MessageBox.Show("密码输入正确");
+                btn_OK.Focus();
+                return;
+            }
+            else
+            {
+                MessageBox.Show("密码输入错误");
+                txt_Regex.Clear();
+                txt_Regex.Focus();
+            }
+           
+        }
+
+
+        /// <summary>
+        /// txt_Regex输入提示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txt_Regex_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txt_Regex.Text, "^[0-9]{0,6}$"))//纯数字
+            {
+               
+                lbl_Tips.Text = "请输入纯数字密码";
+                return;
+            }
+            lbl_Tips.Text = "";
+        }
+
+        /// <summary>
+        /// 按下ENTER 键触发btn_OK事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e.KeyValue =13"></param>
+        private void txt_Regex_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue ==13)//ENTER 键
+            {
+                btn_OK.PerformClick();//触发btn_OK事件
+            }
         }
     }
 }
