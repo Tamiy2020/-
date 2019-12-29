bd92a70f52aad2c3c1529dd4e9a3af3b56c6b833
Tamiy <1145849201@qq.com>
2019-12-29 08:48:36 +0800

    添加“一些方法”项目

---
+++ 一些方法/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace 一些方法
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl1.Location = new System.Drawing.Point(2, 18);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(43, 17);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl2.Location = new System.Drawing.Point(2, 58);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(43, 17);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "label2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(2, 98);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(43, 17);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "label3";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(2, 138);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(43, 17);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 637);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "一些方法";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+    }
+}
+
---
+++ 一些方法/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace 一些方法
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl1.Text = "Directory.GetCurrentDirectory()" + "    " + Directory.GetCurrentDirectory();
+            lbl2.Text = "Application.StartupPath" + "               " + Application.StartupPath;
+            lbl3.Text = "AppDomain.CurrentDomain.BaseDirectory" + "    " + AppDomain.CurrentDomain.BaseDirectory;
+            lbl4.Text = "Assembly.GetExecutingAssembly().Location" + "   " + Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
