30e3b5aee67ff7cbba792e3acd0b1a3e289b7f31
Tamiy <1145849201@qq.com>
2020-01-11 11:33:13 +0800

    添加<数据库操作>项目

---
+++ 数据库操作（SQLite）/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace 数据库操作_SQLite_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_Select = new System.Windows.Forms.TextBox();
+            this.btn_Select = new System.Windows.Forms.Button();
+            this.dgv_Stu = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Stu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Select
+            // 
+            this.txt_Select.Location = new System.Drawing.Point(0, 407);
+            this.txt_Select.Name = "txt_Select";
+            this.txt_Select.Size = new System.Drawing.Size(100, 23);
+            this.txt_Select.TabIndex = 0;
+            // 
+            // btn_Select
+            // 
+            this.btn_Select.Location = new System.Drawing.Point(118, 407);
+            this.btn_Select.Name = "btn_Select";
+            this.btn_Select.Size = new System.Drawing.Size(75, 23);
+            this.btn_Select.TabIndex = 1;
+            this.btn_Select.Text = "查询";
+            this.btn_Select.UseVisualStyleBackColor = true;
+            this.btn_Select.Click += new System.EventHandler(this.btn_Select_Click);
+            // 
+            // dgv_Stu
+            // 
+            this.dgv_Stu.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv_Stu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Stu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgv_Stu.Location = new System.Drawing.Point(0, 0);
+            this.dgv_Stu.Name = "dgv_Stu";
+            this.dgv_Stu.RowTemplate.Height = 23;
+            this.dgv_Stu.Size = new System.Drawing.Size(706, 392);
+            this.dgv_Stu.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 442);
+            this.Controls.Add(this.dgv_Stu);
+            this.Controls.Add(this.btn_Select);
+            this.Controls.Add(this.txt_Select);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "连接数据库操作";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Stu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Select;
+        private System.Windows.Forms.Button btn_Select;
+        private System.Windows.Forms.DataGridView dgv_Stu;
+    }
+}
+
---
+++ 数据库操作（SQLite）/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+
+namespace 数据库操作_SQLite_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Select_Click(object sender, EventArgs e)
+        {
+            //连接数据库
+            string dbPath = AppDomain.CurrentDomain.BaseDirectory + @"db\Stu.db";
+            string connStr = string.Format("Data Source={0};Pooling=true;FailIfMissing=false;password={1}", dbPath, "");
+
+
+
+            //创建连接对象
+            SQLiteConnection conn = new SQLiteConnection(connStr);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            //SQL语句
+            string sqlStr = "select * from students where stu_no like '%" + txt_Select.Text.Trim() + "%'";
+
+
+            //执行查询语句
+            SQLiteDataAdapter da = new SQLiteDataAdapter(sqlStr, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "students");
+
+            //方法一
+            //dgv_Stu.DataSource = ds;
+            //dgv_Stu.DataMember = "students";
+
+            //方法二
+            //dgv_Stu.DataSource = ds.Tables["students"];
+
+            //方法三
+            DataTable dt = ds.Tables["students"];
+            dgv_Stu.DataSource = dt.DefaultView;
+        }
+    }
+}
